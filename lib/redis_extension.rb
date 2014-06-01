aae1786380c5ff5a68e25f832b6d3dbf4bce5cdd
Yehia Abo El-Nga <yehia@devrok.com>
2014-06-01 12:12:27 +0200

    Add RedisExtension functionality

--- lib/redis_extension.rb
+++ lib/redis_extension.rb
@@ -1,5 +1,110 @@
 require "redis_extension/version"
 
+require 'active_support'
+require 'active_record'
+
 module RedisExtension
-  # Your code goes here...
+	extend ActiveSupport::Concern
+	
+	included do
+		after_save {
+			begin
+				key = self.model_key
+				self.class.set_cache(key, self)
+				ap "#{key} re-cached" if defined?(ap)
+			rescue TypeError => e
+				if defined?(ap)
+					ap e
+					ap "We ignore those 'can't dumpt errors' :D"
+				end
+			end
+		}
+
+		after_destroy {
+			key = self.model_key
+			$redis.del(key)
+			# self.class.set_cache(key, self)
+			ap "#{key} cached deleted!" if defined?(ap)
+		}
+
+	    def self.model_key(id, *args)
+	    	key = "#{self.to_s.underscore}:#{id}"
+	    	if args.length > 0
+	    		key << ":" << args.join(':')
+	    	end
+	    	key
+	    end
+
+	    def model_key(*args)
+	    	self.class.model_key(self.id, *args)
+	    end
+
+	    def self.set_cache(key, obj, field=nil)
+	    	if obj.is_a?(Array) || obj.is_a?(ActiveRecord::Relation)
+	    		ap "This is a fucking array" if defined?(ap)
+				obj.each { |item| $redis.zadd(key, item.created_at.to_i, (field ? item[field] : item.id)) }
+	    	else
+	    		$redis.set(key, Marshal.dump(obj))
+	    	end
+	    end
+
+	    def self.get_cache(key, first=0, last=-1)
+	    	if $redis.type(key) == "zset"
+	    		$redis.zrange(key, first, last)
+	    	elsif $redis.type(key) == "string"
+	    		Marshal.load($redis.get(key))
+	    	end
+	    end
+
+	    def self.cache(key, field=nil)
+	    	if !$redis.exists(key)
+	    		obj = yield
+	    		set_cache(key, obj, field)
+	    	end
+
+	    	get_cache(key)
+	    end
+
+	    def self.find(*args)
+	    	key = model_key(args[0])
+	    	cache(key) do
+	    		ap "Caching #{key}" if defined?(ap)
+		    	super(*args)
+		    end
+	    end
+
+	    # def self.find_by(db, args=nil)
+	    # 	args = args ? args.merge!(db: db) : db
+	    # 	super(args)
+	    # end
+
+	    # def self.create(db, args=nil)
+		   #  args = args ? args.merge!(db: db) : db
+		   #  super(args)
+	    # end
+
+	    # def self.where(db, opts=nil, *rest)
+	    # 	if db.is_a?(Integer)
+	    # 		if opts.is_a? Hash
+	    # 			opts.merge!(db: db)
+	    # 		elsif opts.is_a? String
+	    # 			opts << ' AND db = ?'
+	    # 			rest <<  db
+	    # 		end
+	    # 	else
+	    # 		if opts
+	    # 			rest.unshift(opts)
+	    # 		end
+
+	    # 		opts = db
+	    # 	end
+	    	
+	    # 	ap "db: #{db}"
+	    # 	ap "opts: #{opts}"
+	    # 	ap "rest: #{rest}"
+	    	
+	    # 	super(opts, *rest)
+	    # end
+	end
 end
+ActiveRecord::Base.send :include, RedisExtension
